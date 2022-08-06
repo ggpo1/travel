1d2e9d6122bb305f2535dcfe807706614122d596
v.arkhangelskii <johnlevis199@icloud.com>
2022-08-06 19:19:52 +0300

    feat(game): initialize

---
+++ scripts/start_button.gd
@@ -0,0 +1,18 @@
+extends Button
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("hello from button ready");
+
+func _click():
+	print("click");
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
